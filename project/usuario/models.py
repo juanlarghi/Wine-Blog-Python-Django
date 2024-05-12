bef366b7677da4fc0e91e5159e6c131384640514
Juan Larghi <juanlarghi@MacBook-Air.local>
2024-05-12 00:23:00 -0300

    update: usuario

--- project/usuario/models.py
+++ project/usuario/models.py
@@ -1,10 +1,13 @@
 from django.db import models
 
 class Pais(models.Model):
-    nombre = models.CharField(max_length=45)
+    nombre = models.CharField(max_length=45, verbose_name='país')
 
     def __str__(self):
         return self.nombre
+    
+    class Meta:
+        verbose_name_plural = 'países'
 
 class Usuario(models.Model):
     nombre = models.CharField(max_length=45)
--- project/usuario/views.py
+++ project/usuario/views.py
@@ -3,6 +3,6 @@ from django.shortcuts import render
 from . import models
 
 def home(request):
-    ...
-    
-    
+    consulta_usuarios = models.Usuario.objects.all()
+    context = {'usuarios': consulta_usuarios}
+    return render(request, 'usuario/index.html', context)
