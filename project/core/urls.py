31df87a6305c9c4fc4d1235c3d699855949d6079
Juan Larghi <juanlarghi@MacBook-Air.local>
2024-05-19 23:39:55 -0300

    feat: login & logout

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.views import LogoutView
 from django.urls import path
 from . import views
 
@@ -6,4 +7,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.home, name = 'home'),
     path('about/', views.about, name = 'about'),
+    path('login/', views.CustomLoginView.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(template_name="core/logout.html"), name = 'logout'),
 ] 
--- project/core/views.py
+++ project/core/views.py
@@ -1,8 +1,14 @@
+from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from . import models
+from .forms import CustomAuthenticationForm
 
 def home(request):
     return render(request, 'core/index.html')
 
 def about(request):
-    return render(request, 'core/components/about.html' )
+    return render(request, 'core/components/about.html')
+
+class CustomLoginView(LoginView):
+    authentication_form = CustomAuthenticationForm
+    template_name = "core/login.html"
